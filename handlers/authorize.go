bdf8d8a1168206504d751a79d8c84bb1717b8070
Bartlomiej Wilczynski <me@bwilczynski.com>
2020-12-08 22:01:44 +0100

    Get AuthorizeRequest from query params and handle unsupported code challenge types

--- handlers/authorize.go
+++ handlers/authorize.go
@@ -1,6 +1,7 @@
 package handlers
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 )
@@ -9,11 +10,45 @@ type Authorize struct {
 	log *log.Logger
 }
 
+type AuthorizeRequest struct {
+	ClientId            string
+	CodeChallenge       string
+	CodeChallengeMethod string
+	RedirectUri         string
+}
+
+const (
+	CodeChallengeMethodS256 = "S256"
+)
+
+func (ar *AuthorizeRequest) FromQueryParams(r *http.Request) {
+	query := r.URL.Query()
+
+	ar.ClientId = query.Get("client_id")
+	ar.CodeChallenge = query.Get("code_challenge")
+	if codeChallengeMethod := query.Get("code_challenge_method"); codeChallengeMethod != "" {
+		ar.CodeChallengeMethod = codeChallengeMethod
+	} else {
+		ar.CodeChallengeMethod = CodeChallengeMethodS256
+	}
+	ar.RedirectUri = query.Get("redirect_uri")
+}
+
 func NewAuthorize(log *log.Logger) *Authorize {
 	return &Authorize{log}
 }
 
-func (h *Authorize) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
-	h.log.Print("Authorize handler")
-	rw.Write([]byte("authorize"))
+func (h *Authorize) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	ar := &AuthorizeRequest{}
+	ar.FromQueryParams(r)
+
+	h.log.Printf("Authorize handler called: %#v", ar)
+
+	if ar.CodeChallengeMethod != CodeChallengeMethodS256 {
+		h.log.Printf("Code challenge method %v not supported", ar.CodeChallengeMethod)
+		http.Error(w, "Code challenge method not supported", http.StatusBadRequest)
+		return
+	}
+
+	w.Write([]byte(fmt.Sprintf("%#v", ar)))
 }
